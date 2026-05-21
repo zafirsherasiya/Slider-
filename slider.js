45237a43bca7184c28f6e9c27d56d2c1a0ab530c
zafirsherasiya <zafirsci@gmail.com>
2026-05-21 09:49:57 +0530

    Update slider.js

--- slider.js
+++ slider.js
@@ -1,6 +1,6 @@
 const slide = [
-  { id: 1, image: "https://en.wikipedia.org/wiki/Image" },
-  { id: 2, image: "https://www.wearegecko.co.uk/blog/how-to-optimise-images-for-the-web-without-losing-quality/" },
+  { id: 1, image: "https://static.vecteezy.com/system/resources/thumbnails/057/068/323/small/single-fresh-red-strawberry-on-table-green-background-food-fruit-sweet-macro-juicy-plant-image-photo.jpg" },
+  { id: 2, image: "https://img.magnific.com/premium-photo/documentary-filmmaker-capturing-footage-rare-species-bird-wild-realistic-photograp_1101825-72568.jpg?semt=ais_hybrid&w=740&q=80" },
   { id: 3, image: "https://media.istockphoto.com/id/665698842/photo/mountain.jpg?s=612x612&w=is&k=20&c=UYck5fgIyc_rjlHc9Nmq25QV3NEWcrzUi2_OwvoOGJk=" },
 ];
 
